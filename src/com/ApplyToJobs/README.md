c3ea471c2a013e4bcc34328d0757ecf64106edba
Catalyze326 <cfmcool@gmail.com>
2019-05-24 16:59:52 -0400

    Fixed it, cleaned it up and made it work

---
+++ src/com/ApplyToJobs/README.md
@@ -0,0 +1,14 @@
+#Stack Overflow Auto Job Apply
+In order to use this you must download the firefox web driver and put it somewhere in your path. You can find the driver here. https://www.seleniumhq.org/download/ 
+
+You will also need to install selenium with pip
+
+````
+pip3 install selenium
+````
+
+Once you have that in your path and you have installed selenium and you answer its questions you should be able to let it run.
+
+I got bored of applying to jobs so I wrote a program to do it for you. It simply asks for your username, password, work experience level, and filters in a text file and then references them wen looking through sites. I might slow it down so that Stack Overflow does not become suspicious, but right now I think it works.
+
+I am going to build one of thse for Linkedin for all the non programmers who still are lazy and dont want to apply to jobs themselves, but that is to come at a later date.
--- src/com/ApplyToJobs/main.py
+++ src/com/ApplyToJobs/main.py
@@ -1,22 +1,79 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+
 import re
 import os
 import time
 emailPattern = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 experanceLevelPattern = re.compile("^[JjMmSsLl]")
+driver = webdriver.Firefox()
+data = {}
 
 
+def getUserData():
+    if not os.path.isfile('metadata'):
+        f = open("metadata", "w+")
+        email = input('What is the email associated with your Stack Overflow account?')
+        while not emailPattern.match(email):
+            email = input('That is not a valid email address')
+        f.write(email + '\n')
+
+        password = input(
+            'What is the password you use to log into Stack Overflow. Note this is stored in the metadata file and not put anywhere else.\n Feel free to valadate that.')
+        f.write(password + '\n')
+
+        desiredLevel = input(
+            'What is the desired level of experance expectation that a job ought to have for you to apply. Options are Studnet, Junior, Mid-Level, Senior, Lead, or Manager')
+        while not experanceLevelPattern.match(desiredLevel):
+            desiredLevel = input(
+                'That is not a valid experance level. Chose one of the following. Student, Junior, Mid-Level, Senior, Lead, or Manager')
+        f.write(desiredLevel + ',')
+
+        tags = input(
+            'What is a language or framework that you would like to work with?')
+        f.write(tags + '\n')
+        return open('metadata', 'r').readlines()
+    else:
+        return open('metadata', 'r').readlines()
+
+
+def login():
+    driver.get('https://stackoverflow.com/users/login')  # replaces "ie.navigate"
+    driver.find_element_by_id('email').send_keys(data[0][:-1])
+    time.sleep(1)
+    driver.find_element_by_id('password').send_keys(data[1][:-1])
+    driver.find_element_by_id('submit-button').click()
+    driver.get('https://stackoverflow.com/jobs')
+
+
+# TODO close other links
 def recursiveApply(linkNum = 0):
     time.sleep(5)
     if linkNum != 24:
         driver.get('https://stackoverflow.com/jobs/')
         site = driver.find_elements_by_class_name('s-link__visited')[linkNum]
-        print(site)
-        site.click()
-        print(driver.page_source)
-        if data[2].replace(' ', '') in driver.page_source:
+        try:
+            site.click()
+        except ElementClickInterceptedException:
+            print('It failed to click on a new job link')
+            driver.get('https://stackoverflow.com/jobs/')
+            recursiveApply(linkNum + 1)
+        jobInfo = driver.find_elements_by_class_name('fw-bold')
+        moveOn = False
+        for qualitiy in data[2].split(','):
+            for info in jobInfo:
+                if qualitiy.replace(' ', '') in info.text:
+                    moveOn = True
+        if not moveOn:
+            print('The listing did not contain the experience level preference of the user')
+            driver.get('https://stackoverflow.com/jobs/')
+            recursiveApply(linkNum + 1)
+
+        try:
             driver.find_element_by_class_name('_apply').click()
             driver.find_element_by_class_name('j-apply-btn').click()
+        except NoSuchElementException:
+            print('It did not find the second apply button therefore it is not an easy apply site')
         recursiveApply(linkNum + 1)
     else:
         driver.get('https://stackoverflow.com/jobs/')
@@ -24,34 +81,6 @@ def recursiveApply(linkNum = 0):
         recursiveApply(0)
 
 
-if not os.path.isfile('metadata'):
-    f = open("metadata", "w+")
-    email = input('What is the email associated with your Stack Overflow account?')
-    while not emailPattern.match(email):
-        email = input('That is not a valid email address')
-    f.write(email + '\n')
-
-    password = input('What is the password you use to log into Stack Overflow. Note this is stored in the metadata file and not put anywhere else.\n Feel free to valadate that.')
-    f.write(password + '\n')
-
-    desiredLevel = input('What is the desired level of experance expectation that a job ought to have for you to apply. Options are Studnet, Junior, Mid-Level, Senior, Lead, or Manager')
-    while not experanceLevelPattern.match(desiredLevel):
-        desiredLevel = input('That is not a valid experance level. Chose one of the following. Student, Junior, Mid-Level, Senior, Lead, or Manager')
-    f.write(desiredLevel + '\n')
-
-    tags = input('What tags would you like to look fow as you apply for jobs? Some examples are Java, Python, or Bash. Please list these in a coma seperated form')
-    f.write('tags: ' + tags + '\n')
-else:
-    f = open('metadata', 'r').readlines()
-    print(f)
-data = open('metadata', 'r').readlines()
-
-driver = webdriver.Firefox() # Initialize the webdriver session
-driver.get('https://stackoverflow.com/users/login') # replaces "ie.navigate"
-driver.find_element_by_id('email').send_keys(data[0][:-1])
-time.sleep(1)
-driver.find_element_by_id('password').send_keys(data[1][:-1])
-driver.find_element_by_id('submit-button').click()
-
-driver.get('https://stackoverflow.com/jobs')
+data = getUserData()
+login()
 recursiveApply()
