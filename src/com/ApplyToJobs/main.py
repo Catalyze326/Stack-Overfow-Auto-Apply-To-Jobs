c5a304756dd63f9586548abb016353fdf8549406
Catalyze326 <cfmcool@gmail.com>
2019-09-14 05:33:22 -0400

    Total overhaul. Its way better.

--- src/com/ApplyToJobs/main.py
+++ src/com/ApplyToJobs/main.py
@@ -1,9 +1,6 @@
-# TODO add this lang or that lang
-# TODO migrate to json
-
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, NoSuchWindowException
-
+from selenium.webdriver.support.ui import Select
 import re
 import os
 import time
@@ -27,27 +24,39 @@ def getUserData():
         f.write(password + '\n')
         # TODO allow for more than one
         desiredLevel = input(
-            'What is the desired level of experance expectation that a job ought to have for you to apply. '
-            'Options are Studnet, Junior, Mid-Level, Senior, Lead, or Manager. You can pick more than one.')
+            'What is the minimum desired level of experance expectation that a job ought to have for you to apply. '
+            'Options are Studnet, Junior, Mid-Level, Senior, Lead, or Manager. ')
         while not experanceLevelPattern.match(desiredLevel):
-            desiredLevels = input(
+            desiredLevel = input(
+                'That is not a valid experance level. Chose one of the following. Student, Junior, Mid-Level, Senior, Lead, or Manager. Be sure to capatlize the first letter of the level you chose.')
+        f.write(desiredLevel + '\n')
+
+        desiredLevel = input(
+            'What is the maximum desired level of experance expectation that a job ought to have for you to apply. '
+            'Options are Studnet, Junior, Mid-Level, Senior, Lead, or Manager. ')
+        while not experanceLevelPattern.match(desiredLevel):
+            desiredLevel = input(
                 'That is not a valid experance level. Chose one of the following. Student, Junior, Mid-Level, Senior, Lead, or Manager. Be sure to capatlize the first letter of the level you chose.')
         f.write(desiredLevel + '\n')
 
         tags = input(
-            'What are some language or framework that must be a part of the job you want to apply for?')
+            'What are some technologies you like? Separate them by spaces.')
         f.write(tags + '\n')
 
         langs = input(
-            'What are some languages or frameworks you would like to NOT work with. Put these in a coma seperated list? Be sure to capatalize the first letter of each word')
+            "What are some languages you don't like? Separate by spaces.")
         f.write(langs)
+
+        salary = input(
+            "What is your desired salary?.")
+        f.write(salary)
         return open('metadata', 'r').readlines()
     else:
         return open('metadata', 'r').readlines()
 
 
 def login():
-    driver.get('https://stackoverflow.com/users/login')  # replaces "ie.navigate"
+    driver.get('https://stackoverflow.com/users/login')
     driver.find_element_by_id('email').clear()
     driver.find_element_by_id('email').send_keys(data[0][:-1])
     time.sleep(1)
@@ -55,6 +64,8 @@ def login():
     driver.find_element_by_id('password').send_keys(data[1][:-1])
     driver.find_element_by_id('submit-button').click()
     driver.get('https://stackoverflow.com/jobs')
+    time.sleep(1)
+
 
 
 # TODO close other links
@@ -63,6 +74,17 @@ def recursiveApply(linkNum = 0):
     time.sleep(15.12)
     # TODO get back to the jobs listing without going back to page 0
     driver.get('https://stackoverflow.com/jobs')
+    params = driver.find_elements_by_class_name('js-tab-wrapper')
+    params[0].find_element_by_xpath(".//*[@href='#']").click()
+    driver.find_element_by_id('tageditor-replacing-tl--input').send_keys(data[5])
+    driver.find_element_by_id('tageditor-replacing-td--input').send_keys(data[4])
+    params[1].find_element_by_xpath(".//*[@href='#']").click()
+    driver.find_element_by_id('s').send_keys(data[6].replace('\n', '').replace(',', ''))
+    params[3].find_element_by_xpath(".//*[@href='#']").click()
+    Select(driver.find_element_by_id('ms')).select_by_visible_text(data[2].replace('\n', ''))
+    Select(driver.find_element_by_id('mxs')).select_by_visible_text(data[3].replace('\n', ''))
+    params[0].find_element_by_xpath(".//*[@href='#']").click()
+    driver.find_element_by_class_name('btn').click()
     for i in range(int(linkNum / 23)):
         driver.find_element_by_class_name('test-pagination-next').click()
         time.sleep(5)
@@ -77,37 +99,19 @@ def recursiveApply(linkNum = 0):
     time.sleep(1)
     driver.execute_script('document.getElementsByClassName("sidebar")[0].remove();')
     driver.execute_script('document.getElementById("more-jobs-items").remove();')
-    pureHTML = driver.page_source
     jobTitle = driver.find_element_by_class_name('fc-black-900').text
     print('Looking at job listing "' + jobTitle + '"')
-    moveOn = False
-    # Things you want in every posting
-    for qualitiy in data[2].split(','):
-        if qualitiy.lower() in str(pureHTML).lower():
-            print('Listing has: ' + qualitiy)
-            moveOn = True
-    if not moveOn:
-        print('The job listing is missing some quality you required.')
+    try:
+        driver.find_element_by_class_name('_apply').click()
+        driver.find_element_by_class_name('j-apply-btn').click()
+        print('We applied to a job titled: ' + jobTitle)
+        open('JobsAppliedTo.txt', 'a+').write(jobTitle + '        ' + driver.current_url + '\n')
         recursiveApply(linkNum + 1)
-    # Things to avoid in a posting
-    for qualitiy in data[3].split(','):
-        if qualitiy.lower() in str(pureHTML).lower():
-            print('Found quality: ' + qualitiy + ' which we are avoiding.')
-            recursiveApply(linkNum + 1)
-    else:
-        try:
-            driver.find_element_by_class_name('_apply').click()
-            driver.find_element_by_class_name('j-apply-btn').click()
-            print('We applied to a job titled: ' + jobTitle)
-            open('JobsAppliedTo.txt', 'a+').write(jobTitle + '        ' + driver.current_url + '\n')
-            recursiveApply(linkNum + 1)
-        except NoSuchElementException:
-            print('It was not a proper form easy apply site')
-            # driver.switch_to.window(driver.window_handles[1])
-            # driver.close()
-            recursiveApply(linkNum + 1)
-        except NoSuchWindowException:
-            recursiveApply(linkNum + 1)
+    except NoSuchElementException:
+        print('It was not a proper form easy apply site')
+        recursiveApply(linkNum + 1)
+    except NoSuchWindowException:
+        recursiveApply(linkNum + 1)
 
 
 data = getUserData()
